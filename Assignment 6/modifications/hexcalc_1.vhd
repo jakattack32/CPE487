7b22c2c744ef926db40078aa7dbbe83c27602fc2
Jonah Kramer <90064335+jakattack32@users.noreply.github.com>
2022-03-29 12:58:16 -0400

    Update hexcalc_1.vhd

--- Assignment 6/modifications/hexcalc_1.vhd
+++ Assignment 6/modifications/hexcalc_1.vhd
@@ -1,43 +1,139 @@
-  
-## Clock signal
-set_property -dict { PACKAGE_PIN E3    IOSTANDARD LVCMOS33 } [get_ports { clk_50MHz }]; #IO_L12P_T1_MRCC_35 Sch=clk100mhz
-create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {clk_50MHz}];
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-##Buttons
+ENTITY hexcalc IS
+	PORT (
+		clk_50MHz : IN STD_LOGIC; -- system clock (50 MHz)
+		SEG7_anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); -- anodes of eight 7-seg displays
+		SEG7_seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0); -- common segments of 7-seg displays
+		bt_clr : IN STD_LOGIC; -- calculator "clear" button
+		bt_plus : IN STD_LOGIC; -- calculator "+" button
+		bt_eq : IN STD_LOGIC; -- calculator "=" button
+		KB_col : OUT STD_LOGIC_VECTOR (4 DOWNTO 1); -- keypad column pins
+		KB_row : IN STD_LOGIC_VECTOR (4 DOWNTO 1); -- keypad row pins
+		bt_sub : IN STD_LOGIC);
+END hexcalc;
 
-#set_property -dict { PACKAGE_PIN C12   IOSTANDARD LVCMOS33 } [get_ports { CPU_RESETN }]; #IO_L3P_T0_DQS_AD1P_15 Sch=cpu_resetn
-
-set_property -dict { PACKAGE_PIN N17   IOSTANDARD LVCMOS33 } [get_ports { bt_clr }]; #IO_L9P_T1_DQS_14 Sch=btnc
-set_property -dict { PACKAGE_PIN M18   IOSTANDARD LVCMOS33 } [get_ports { bt_plus }]; #IO_L4N_T0_D05_14 Sch=btnu
-set_property -dict { PACKAGE_PIN P17   IOSTANDARD LVCMOS33 } [get_ports { bt_eq }]; #IO_L12P_T1_MRCC_14 Sch=btnl
-#set_property -dict { PACKAGE_PIN M17   IOSTANDARD LVCMOS33 } [get_ports { BTNR }]; #IO_L10N_T1_D15_14 Sch=btnr
-set_property -dict { PACKAGE_PIN P18   IOSTANDARD LVCMOS33 } [get_ports { bt_sub }]; #IO_L9N_T1_DQS_D13_14 Sch=btnd
-
-##Pmod Header JA
-
-set_property -dict { PACKAGE_PIN C17   IOSTANDARD LVCMOS33 } [get_ports { KB_col[4] }]; #IO_L20N_T3_A19_15 Sch=ja[1]
-set_property -dict { PACKAGE_PIN D18   IOSTANDARD LVCMOS33 } [get_ports { KB_col[3] }]; #IO_L21N_T3_DQS_A18_15 Sch=ja[2]
-set_property -dict { PACKAGE_PIN E18   IOSTANDARD LVCMOS33 } [get_ports { KB_col[2] }]; #IO_L21P_T3_DQS_15 Sch=ja[3]
-set_property -dict { PACKAGE_PIN G17   IOSTANDARD LVCMOS33 } [get_ports { KB_col[1] }]; #IO_L18N_T2_A23_15 Sch=ja[4]
-set_property -dict { PACKAGE_PIN D17   IOSTANDARD LVCMOS33 } [get_ports { KB_row[4] }]; #IO_L16N_T2_A27_15 Sch=ja[7]
-set_property -dict { PACKAGE_PIN E17   IOSTANDARD LVCMOS33 } [get_ports { KB_row[3] }]; #IO_L16P_T2_A28_15 Sch=ja[8]
-set_property -dict { PACKAGE_PIN F18   IOSTANDARD LVCMOS33 } [get_ports { KB_row[2] }]; #IO_L22N_T3_A16_15 Sch=ja[9]
-set_property -dict { PACKAGE_PIN G18   IOSTANDARD LVCMOS33 } [get_ports { KB_row[1] }]; #IO_L22P_T3_A17_15 Sch=ja[10]
-
-##7-Segment Display
-set_property -dict {PACKAGE_PIN L18 IOSTANDARD LVCMOS33} [get_ports {SEG7_seg[0]}]
-set_property -dict {PACKAGE_PIN T11 IOSTANDARD LVCMOS33} [get_ports {SEG7_seg[1]}]
-set_property -dict {PACKAGE_PIN P15 IOSTANDARD LVCMOS33} [get_ports {SEG7_seg[2]}]
-set_property -dict {PACKAGE_PIN K13 IOSTANDARD LVCMOS33} [get_ports {SEG7_seg[3]}]
-set_property -dict {PACKAGE_PIN K16 IOSTANDARD LVCMOS33} [get_ports {SEG7_seg[4]}]
-set_property -dict {PACKAGE_PIN R10 IOSTANDARD LVCMOS33} [get_ports {SEG7_seg[5]}]
-set_property -dict {PACKAGE_PIN T10 IOSTANDARD LVCMOS33} [get_ports {SEG7_seg[6]}]
-
-set_property -dict {PACKAGE_PIN U13 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[7]}]
-set_property -dict {PACKAGE_PIN K2 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[6]}]
-set_property -dict {PACKAGE_PIN T14 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[5]}]
-set_property -dict {PACKAGE_PIN P14 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[4]}]
-set_property -dict {PACKAGE_PIN J14 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[3]}]
-set_property -dict {PACKAGE_PIN T9 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[2]}]
-set_property -dict {PACKAGE_PIN J18 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[1]}]
-set_property -dict {PACKAGE_PIN J17 IOSTANDARD LVCMOS33} [get_ports {SEG7_anode[0]}]
+ARCHITECTURE Behavioral OF hexcalc IS
+	COMPONENT keypad IS
+		PORT (
+			samp_ck : IN STD_LOGIC;
+			col : OUT STD_LOGIC_VECTOR (4 DOWNTO 1);
+			row : IN STD_LOGIC_VECTOR (4 DOWNTO 1);
+			value : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+			hit : OUT STD_LOGIC
+		);
+	END COMPONENT;
+	COMPONENT leddec16 IS
+		PORT (
+			dig : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+			data : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+			anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+			seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+		);
+	END COMPONENT;
+	SIGNAL cnt : std_logic_vector(20 DOWNTO 0); -- counter to generate timing signals
+	SIGNAL kp_clk, kp_hit, sm_clk : std_logic;
+	SIGNAL kp_value : std_logic_vector (3 DOWNTO 0);
+	SIGNAL nx_acc, acc : std_logic_vector (15 DOWNTO 0); -- accumulated sum
+	SIGNAL nx_operand, operand : std_logic_vector (15 DOWNTO 0); -- operand
+	SIGNAL display : std_logic_vector (15 DOWNTO 0); -- value to be displayed
+	SIGNAL led_mpx : STD_LOGIC_VECTOR (2 DOWNTO 0); -- 7-seg multiplexing clock
+	TYPE state IS (ENTER_ACC, ACC_RELEASE, START_OP, OP_RELEASE, 
+	ENTER_OP, SHOW_RESULT); -- state machine states
+	SIGNAL pr_state, nx_state : state; -- present and next states
+	SIGNAL plus_or_sub: STD_LOGIC;
+BEGIN
+	ck_proc : PROCESS (clk_50MHz)
+	BEGIN
+		IF rising_edge(clk_50MHz) THEN -- on rising edge of clock
+			cnt <= cnt + 1; -- increment counter
+		END IF;
+	END PROCESS;
+	kp_clk <= cnt(15); -- keypad interrogation clock
+	sm_clk <= cnt(20); -- state machine clock
+	led_mpx <= cnt(19 DOWNTO 17); -- 7-seg multiplexing clock
+	kp1 : keypad
+	PORT MAP(
+		samp_ck => kp_clk, col => KB_col, 
+		row => KB_row, value => kp_value, hit => kp_hit
+		);
+		led1 : leddec16
+		PORT MAP(
+			dig => led_mpx, data => display, 
+			anode => SEG7_anode, seg => SEG7_seg
+		);
+		sm_ck_pr : PROCESS (bt_clr, sm_clk) -- state machine clock process
+		BEGIN
+			IF bt_clr = '1' THEN -- reset to known state
+				acc <= X"0000";
+				operand <= X"0000";
+				pr_state <= ENTER_ACC;
+			ELSIF rising_edge (sm_clk) THEN -- on rising clock edge
+				pr_state <= nx_state; -- update present state
+				acc <= nx_acc; -- update accumulator
+				operand <= nx_operand; -- update operand
+			END IF;
+		END PROCESS;
+		-- state maching combinatorial process
+		-- determines output of state machine and next state
+		sm_comb_pr : PROCESS (kp_hit, kp_value, bt_plus, bt_eq, acc, operand, pr_state)
+		BEGIN
+			nx_acc <= acc; -- default values of nx_acc, nx_operand & display
+			nx_operand <= operand;
+			display <= acc;
+			CASE pr_state IS -- depending on present state...
+				WHEN ENTER_ACC => -- waiting for next digit in 1st operand entry
+					IF kp_hit = '1' THEN
+						nx_acc <= acc(11 DOWNTO 0) & kp_value;
+						nx_state <= ACC_RELEASE;
+					ELSIF bt_plus = '1' THEN
+						nx_state <= START_OP;
+						plus_or_sub <= '1';
+					ELSIF bt_sub ='1'then
+					   nx_state <= START_OP;
+					   plus_or_sub <='0';
+					ELSE
+						nx_state <= ENTER_ACC;
+					END IF;
+				WHEN ACC_RELEASE => -- waiting for button to be released
+					IF kp_hit = '0' THEN
+						nx_state <= ENTER_ACC;
+					ELSE nx_state <= ACC_RELEASE;
+					END IF;
+				WHEN START_OP => -- ready to start entering 2nd operand
+					IF kp_hit = '1' THEN
+						nx_operand <= X"000" & kp_value;
+						nx_state <= OP_RELEASE;
+						display <= operand;
+					ELSE nx_state <= START_OP;
+					END IF;
+				WHEN OP_RELEASE => -- waiting for button ot be released
+					display <= operand;
+					IF kp_hit = '0' THEN
+						nx_state <= ENTER_OP;
+					ELSE nx_state <= OP_RELEASE;
+					END IF;
+				WHEN ENTER_OP => -- waiting for next digit in 2nd operand
+					display <= operand;
+					IF (bt_eq = '1' and plus_or_sub='1') THEN
+						nx_acc <= acc + operand;
+						nx_state <= SHOW_RESULT;
+					ELSIF (bt_eq = '1'and plus_or_sub= '0')then
+					   nx_acc <= acc - operand;
+					   nx_state <= SHOW_RESULT;
+					ELSIF kp_hit = '1' THEN
+						nx_operand <= operand(11 DOWNTO 0) & kp_value;
+						nx_state <= OP_RELEASE;
+					ELSE nx_state <= ENTER_OP;
+					END IF;
+				WHEN SHOW_RESULT => -- display result of addition
+					IF kp_hit = '1' THEN
+						nx_acc <= X"000" & kp_value;
+						nx_state <= ACC_RELEASE;
+					ELSE nx_state <= SHOW_RESULT;
+					END IF;
+			END CASE;
+		END PROCESS;
+END Behavioral;
